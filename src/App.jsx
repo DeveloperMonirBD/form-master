be099cfb83f9d7ac561c4ce351fd1f0721ec374d
Monir <mrmonir0558@gmail.com>
2024-10-31 00:05:13 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import RefForm from './components/RefForm/RefForm'
+import SimpoleForm from './components/SimplaForm/SimpoleForm'
+import StateFulForm from './components/StatefulForm/StateFulForm'
+
+function App() {
+
+
+  return (
+    <>
+      {/* <SimpoleForm /> */}
+      {/* <StateFulForm /> */}
+      <RefForm />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/SimplaForm/SimpoleForm.jsx
@@ -0,0 +1,24 @@
+
+
+const SimpoleForm = () => {
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        const data = e.target.name.value
+        const email = e.target.email.value
+        console.log(data, email);
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" />
+                <br />
+                <input type="email" name="email" id=""/>
+                <br />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    );
+};
+
+export default SimpoleForm;
